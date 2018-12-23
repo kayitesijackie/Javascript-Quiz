c4026463b55d2821568e602234ed160bc30236e0
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2018-12-23 12:07:15 +0200

    update file

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
     var q4= $("input:radio[name=qst4]:checked").val();
     var q5= $("input:radio[name=qst5]:checked").val();
     var result=parseInt(q1)+parseInt(q2)+parseInt(q3)+parseInt(q4)+parseInt(q5);
-    $("#result").text("YOU SCORED: " +result + "%");
+    $("#result").text("YOU SCORED IS: " +result + "%");
 
     $("button").click(function() {
-        $("#result-showing").toggle();
-        $("#form-hidden").toggle();
+        $("#result").toggle();
+        $("#form").toggle();
     });
     $("form#form").hide();
     $("#result").show();
